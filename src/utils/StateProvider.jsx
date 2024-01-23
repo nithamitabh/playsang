3bbc588655ed2b43a63941b923cbb40289e0918a
= <21dec004@nith.ac.in>
2024-01-23 17:10:29 +0530

    Remove unnecessary test files and dependencies, update App component

---
+++ src/utils/StateProvider.jsx
@@ -0,0 +1,13 @@
+//reducer hook and context api
+import { createContext,useContext,useReducer } from "react";
+
+export const StateContext = createContext();
+
+
+export const StateProvider = ({children, initialState, reducer}) => (
+    <StateContext.Provider value={useReducer(reducer,initialState)}>
+        {children}
+    </StateContext.Provider>
+);
+
+export const useStateValue = () => useContext(StateContext);
---
+++ src/utils/reducer.js
@@ -0,0 +1,11 @@
+export const initialState = {
+    token: null,
+};
+
+const reducer = (state,action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+};
+export default reducer;
